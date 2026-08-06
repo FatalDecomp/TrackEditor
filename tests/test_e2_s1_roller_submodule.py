853da52241bc148b13228dd11df05f40c435caab
Zizin13 <162662805+Zizin13@users.noreply.github.com>
2026-08-05 23:36:01 -0700

    updating roller pin

--- tests/test_e2_s1_roller_submodule.py
+++ tests/test_e2_s1_roller_submodule.py
@@ -10,7 +10,7 @@ import unittest
 
 REPOSITORY_ROOT = Path(__file__).resolve().parents[1]
 ROLLER_ROOT = REPOSITORY_ROOT / "external" / "ROLLER"
-EXPECTED_ROLLER_COMMIT = "affa990d09ac1d0ae595910e929d112843264ad9"
+EXPECTED_ROLLER_COMMIT = "1b5665ac84d0923b2bc978405e5c383b9b9dec12"
 
 
 def run_git(*arguments: str, cwd: Path = REPOSITORY_ROOT) -> subprocess.CompletedProcess:
